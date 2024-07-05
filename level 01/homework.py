131788f9e86d89c031e59d5099ade0c3eab0c14c
tedomalakmadze <guesthousegulo@gmail.com>
2024-07-05 16:21:33 +0400

    07,05

---
+++ level 01/homework.py
@@ -0,0 +1,34 @@
+neme = "nika"
+#name არის ცვლადი
+# = არის ცვლადისტვის მნიშვნელობის მიმნიჭებელი სიმბილო
+# "nika" არის ცვლადისთვის მნიშვნელობა
+
+surname = "keshelava"
+
+ # print(neme)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+neme = "nika"  #ეს არის str (string) ტიპის ცვლადი 
+age = 22 # ეს არის int ( integer ) მტელი-რიცხვი
+height = 184.5 #ეს არის fioat ცვლაცი ( ატწილადი )
+ # boolean (bool) ცვლადი
+
+knows_programming = True  #True ან False
+is_ugly = False # snakecase ( უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False # ჯავასკრიპტული camelcase
+
+
+# print(name + " " + surname)
+
+# # სტრინგი არის ბრჭყალებშ მოქცეული სიმბოლოები
+# print(name + age) 
+
+#print(type(age))
+#print(type(neme))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(neme + " " + str (age ))
+
